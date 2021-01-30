213e47b9b95f175b72c946b382cc89ba8db0249f
Andrew Clarke <mail@ozzmosis.com>
2021-01-30 19:52:05 +1100

    "Hello world" in Java

---
+++ java/hello.java
@@ -0,0 +1,17 @@
+//  "Hello world" for Java.
+//
+//  Build with:
+//
+//    $ javac hello.java
+//
+//  Run with:
+//
+//    $ java hello
+
+public class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world.");
+    }
+}
