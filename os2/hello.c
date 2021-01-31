a2dbf6f7a26e16105f7bc0eb2a1ad47d81b17f8d
Andrew Clarke <mail@ozzmosis.com>
2021-02-01 09:30:55 +1100

    Tweaks to build for both 16-bit & 32-bit OS/2

---
+++ os2/hello.c
@@ -0,0 +1,22 @@
+/*
+ *  "Hello world" in C using the OS/2 API.
+ */
+
+#include <stdio.h>
+
+#define INCL_DOSPROCESS
+#include <os2.h>
+
+static char msg[] = "Hello world.\n";
+
+int main(void)
+{
+#ifdef __386__
+    ULONG written;
+#else
+    USHORT written;
+#endif
+
+    DosWrite(1, msg, sizeof msg, &written);
+    return 0;
+}
