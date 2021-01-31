a073cb7b88aa496d6e6494cfbb46a76520119c05
Andrew Clarke <mail@ozzmosis.com>
2021-01-31 11:47:10 +1100

    "Hello world" in Microsoft C++/CLI for the .NET Framework.

---
+++ c++cli/hello.cpp
@@ -0,0 +1,13 @@
+//
+// "Hello world" in Microsoft C++/CLI for the .NET Framework.
+//
+
+#using <mscorlib.dll>
+#using <system.dll>
+
+using namespace System;
+
+void main(int argc ,char *argv[])
+{
+    Console::WriteLine("Hello world.");
+}
