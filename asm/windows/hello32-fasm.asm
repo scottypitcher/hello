aa289bfe98e4345c5acb7c82be1089713a204fb5
Andrew Clarke <mail@ozzmosis.com>
2021-02-04 04:31:12 +1100

    "Hello world" in 80386 assembly for 32-bit Windows NT

---
+++ asm/windows/hello32-fasm.asm
@@ -0,0 +1,39 @@
+;
+;  "Hello world" for Intel 80386 32-bit Windows NT.
+;
+;  Written for the Flat Assembler (FASM) from https://flatassembler.net/
+;
+;  Requires win32a.inc and related files, supplied with the Windows
+;  distribution of FASM.
+;
+;  Build HELLO32.EXE with:
+;
+;  set INCLUDE=c:\fasm\include
+;  fasm hello32-fasm.asm hello32.exe
+;
+;  It's possible to cross-compile from Linux/BSD, etc. Ensure INCLUDE
+;  points to the location of win32a.inc.
+;
+
+format pe console
+
+include 'win32a.inc'
+
+entry _start
+
+_start:
+    invoke GetStdHandle, STD_OUTPUT_HANDLE
+    invoke WriteFile, eax, msg, msg.len, 0, 0
+    invoke ExitProcess, 0
+
+section '.idata' import data readable writeable
+    library kernel, 'KERNEL32.DLL'
+
+    import kernel, \
+      WriteFile, 'WriteFile', \
+      GetStdHandle, 'GetStdHandle', \
+      ExitProcess, 'ExitProcess'
+
+section '.data' data readable writeable
+    msg db "Hello world.", 0Dh, 0Ah
+    .len = ($-msg)
---
+++ asm/windows/hello32.asm
@@ -0,0 +1,56 @@
+;
+;  "Hello world" for Intel 80386 32-bit Windows NT.
+;
+;  Written for the Netwide Assembler (NASM) from http://www.nasm.us/
+;  and LINK.EXE from Microsoft Visual C++ (or Visual Studio).
+;
+;  Build HELLO32.EXE with:
+;
+;  nasm -fwin32 hello32.asm
+;  link /subsystem:console /nodefaultlib /entry:_start hello32.obj kernel32.lib
+;
+;  Optional:
+;
+;  Add "/align:16" to the LINK command above to shrink HELLO32.EXE
+;  from 3072 bytes down to 784.
+;
+
+section .data
+
+STD_OUTPUT_HANDLE equ -11
+
+msg:
+    db 'Hello world.', 0x0A
+    .len: equ $-msg
+
+global _start
+
+extern  _GetStdHandle@4
+extern  _WriteFile@20
+extern  _ExitProcess@4
+
+section .text
+
+_start:
+    ; establish a new stack frame
+    mov ebp, esp
+    sub esp, 4
+
+    ; HANDLE WINAPI GetStdHandle(nStdHandle)
+    push STD_OUTPUT_HANDLE        ; nStdHandle
+    call _GetStdHandle@4          ; GetStdHandle()
+
+    ; the return value of GetStdHandle() is in EAX
+
+    ; BOOL WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, lpNumberOfBytesWritten, lpOverlapped)
+    ; Note: Parameters are pushed in reverse order.
+    push 0                        ; lpOverlapped
+    push 0                        ; lpNumberOfBytesWritten (set to NULL)
+    push msg.len                  ; nNumberOfBytesToWrite
+    push msg                      ; lpBuffer
+    push eax                      ; hFile; EAX still has the result from GetStdHandle()
+    call _WriteFile@20            ; WriteFile()
+
+    ; VOID ExitProcess(uExitCode)
+    push 0                        ; uExitCode
+    call _ExitProcess@4           ; ExitProcess()
