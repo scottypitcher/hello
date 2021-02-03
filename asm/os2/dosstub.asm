75b0f69909910daf6cb7f617708de3d6034b9280
Andrew Clarke <mail@ozzmosis.com>
2021-02-04 02:47:23 +1100

    DOSSTUB.EXE assembly for Microsoft MASM

--- asm/os2/dosstub.asm
+++ asm/os2/dosstub.asm
@@ -1,24 +1,39 @@
 ;
-;  TODO header
+;  DOS stub for OS/2 executables.
 ;
+;  Displays "This program cannot be run in a DOS session." and exits.
+;
+;  Requires Microsoft Macro Assembler. Tested with MASM 6.00B.
+;
+;  Build DOSSTUB.EXE with:
+;
+;  ml -c dosstub.asm
+;  link dosstub.obj,,,,,
 
 .model small
-.stack 256
+.stack 100h
 .code
 
-; TODO consts
+_DOSWRITETOSTDOUT = 09h
+_DOSEXIT          = 4Ch
 
-; TODO func macros
-
-_start:
+DosWriteToStdOut macro string
     mov ax, @data
     mov ds, ax
-    lea dx, msg
-    mov ah, 09h
+    lea dx, string
+    mov ah, _DOSWRITETOSTDOUT
     int 21h
+endm
 
-    mov ax, 4C00h
+DosExit macro retval
+    mov ah, _DOSEXIT
+    mov al, retval
     int 21h
+endm
+
+_start:
+    DosWriteToStdout msg
+    DosExit 0
 
 .data
     msg byte "This program cannot be run in a DOS session.", 0Dh, 0Dh, 0Ah, "$"
