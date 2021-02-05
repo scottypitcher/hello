5172527e338cf2c1d63197c3568fddee92fa1808
Andrew Clarke <mail@ozzmosis.com>
2021-02-05 21:11:59 +1100

    Bug fix

--- asm/darwin/helloppc.asm
+++ asm/darwin/helloppc.asm
@@ -2,6 +2,8 @@
 ;  "Hello world" on 32-bit PowerPC for Apple OS X (Darwin).
 ;
 ;  Via https://en.wikibooks.org/wiki/Computer_Programming/Hello_world#RISC_processor:_PowerPC.2C_Mac_OS_X.2C_GAS
+;  Via http://index-of.es/Exploit/PowerPC-OS%20X%20(Darwin)%20Shellcode%20Assembly.pdf
+;  Via http://uninformed.org/?v=1&a=1&t=pdf  "Mac OS X PPC Shellcode Tricks" by H D Moore
 ;
 ;
 ;  Build with:
@@ -10,6 +12,14 @@
 ;  ld -o helloppc helloppc.o
 ;
 
+.data
+
+.align 2
+
+msg:
+    .ascii "Hello world.\n"
+    msglen = .-msg
+
 .set SYS_WRITE, 4
 .set SYS_EXIT,  1
 
@@ -17,26 +27,30 @@
 .set stdout, 1
 .set stderr, 2
 
-.data
-
-msg:
-    .ascii "Hello world.\n"
-    msglen = .-msg
- 
 .text
+
+.align 2
 
 .globl _main
 
 _main:
     ; write(stdout, msg, msglen)
-    li r5, msglen
-    addis r4, 0, ha16(msg)   ; load high 16 bits of address of output string
-    addi r4, r4, lo16(msg)   ; load low 16 bits of address of output string
-    li r3, stdout
     li r0, SYS_WRITE
-    sc                       ; syscall
- 
+    li r3, stdout
+    lis r4, ha16(msg)
+    ori r4, r4, lo16(msg)
+    li r5, msglen
+    sc  ; syscall
+
+    ;  "An interesting feature of Mac OS X [on PowerPC] is that a
+    ;  successful system call will return to the address 4 bytes
+    ;  after the end of ’sc’ instruction and a failed system call
+    ;  will return directly after the ’sc’ instruction. This allows
+    ;  you to execute a specific instruction only when the system
+    ;  call fails." - H D Moore
+
     ; exit(0)
-    li r3, 0
+    nop
     li r0, SYS_EXIT
+    li r3, 0
     sc
