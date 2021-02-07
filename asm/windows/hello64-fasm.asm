2fed4d124e0db68210366d24514d1a40d3b097cd
Andrew Clarke <mail@ozzmosis.com>
2021-02-08 01:09:53 +1100

    "Hello world" in AMD64 assembler for 64-bit Windows NT

---
+++ asm/windows/hello64-fasm.asm
@@ -0,0 +1,38 @@
+;
+;  "Hello world" for AMD64 64-bit Windows NT.
+;
+;  Written for the Flat Assembler (FASM) from https://flatassembler.net/
+;
+;  Requires win32a.inc and related files, supplied with the Windows
+;  distribution of FASM.
+;
+;  Build HELLO64.EXE with:
+;
+;  set INCLUDE=c:\fasm\include
+;  fasm hello64-fasm.asm hello64.exe
+;
+;  It's possible to cross-compile from Linux/BSD, etc. Ensure INCLUDE
+;  points to the location of win32a.inc.
+;
+
+format pe64 console
+
+include 'win64a.inc'
+
+entry _start
+
+section '.text' code readable executable
+
+_start:
+    sub rsp, 8 * 5    ; reserve stack for API use and make stack dqword aligned
+    invoke GetStdHandle, STD_OUTPUT_HANDLE
+    invoke WriteFile, eax, msg, msg.len, 0, 0
+    invoke ExitProcess, 0
+
+section '.idata' import data readable writeable
+    library kernel32, 'KERNEL32.DLL'
+    include 'api/kernel32.inc'
+
+section '.data' data readable writeable
+    msg db "Hello world.", 0Dh, 0Ah
+    .len = ($-msg)
