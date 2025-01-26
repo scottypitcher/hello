01bc97b2bab22dc3df5bcd3b430b9c8012a84c29
Andrew Clarke <mail@ozzmosis.com>
2025-01-27 03:45:15 +1100

    Use // for comments & fix build instructions

--- asm/linux/helloarm.asm
+++ asm/linux/helloarm.asm
@@ -1,14 +1,11 @@
-@
-@  "Hello world" on Linux ARM 32-bit.
-@
-@  In ARM assembly syntax for GNU as, @ is used for comments, because # is
-@  used to specify decimal numbers.
-@
-@  Build with:
-@
-@  as -o helloarm.o helloarm.s
-@  ld -s helloarm.o
-@
+//
+//  "Hello world" on Linux ARM 32-bit.
+//
+//  Build with:
+//
+//  arm-linux-gnueabi-as -o helloarm.o helloarm.asm
+//  arm-linux-gnueabi-ld -o helloarm helloarm.o -s
+//
 
 .set SYS_WRITE, 4
 .set SYS_EXIT,  1
@@ -28,13 +25,13 @@ msg:
 .global _start
 
 _start:
-    mov r7, #SYS_WRITE  @ write()
-    mov r0, #stdout     @ stdout
-    ldr r1, =msg        @ string text
-    ldr r2, =msglen     @ string length
-    swi 0               @ software interrupt
+    mov r7, #SYS_WRITE  // write()
+    mov r0, #stdout     // stdout
+    ldr r1, =msg        // string text
+    ldr r2, =msglen     // string length
+    swi 0               // software interrupt
 
 _exit:
-    mov r7, #SYS_EXIT   @ exit()
-    mov r0, #0          @ return code
-    swi 0               @ software interrupt
+    mov r7, #SYS_EXIT   // exit()
+    mov r0, #0          // return code
+    swi 0               // software interrupt
